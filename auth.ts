01b2f3ac4ab9f0baae894509fa0eb45b899457c3
brandmoredev <brandmoredev@gmail.com>
2024-08-30 23:33:03 +0300

    Adds user role and extended session and jwt type

--- auth.ts
+++ auth.ts
@@ -8,6 +8,8 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import { db } from "@/lib/db";
 import authConfig from "@/auth.config";
 
+import { getUserById } from "@/utils/data/user";
+
 /**
  * Configures NextAuth.js with Prisma adapter and JWT session strategy.
  * 
@@ -32,7 +34,40 @@ import authConfig from "@/auth.config";
  * ```
  */
 export const { auth, handlers, signIn, signOut } = NextAuth({
-  ...authConfig,
+  callbacks: {
+    async signIn({ user }) {
+      // if (user.id) {
+      //   const existingUser = await getUserById(user.id)
+
+      //   if (!existingUser || !existingUser.emailVerified) {
+      //     return false
+      //   }
+      // }
+
+      return true;
+    },
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub
+      }
+      if (token.role && session.user) {
+        session.user.role = token.role
+      }
+
+      return session;
+    },
+    async jwt({ token }) {
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) return token;
+
+      token.role = existingUser.role;
+      return token
+    }
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
+  ...authConfig,
 });
